4401977eb71f2dd2a8604aa6e5db840385bfe3a8
khalid-you <76524196+khalid-you@users.noreply.github.com>
2021-01-20 09:06:32 -0800

    Add files via upload

---
+++ objects_methods.js
@@ -0,0 +1,22 @@
+/*
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birth_year: 1991,
+    job: 'techear',
+    friends: ['Micheal', 'Peter', 'Steven'],
+    has_drivers_licenes: true,
+    /*
+    calcAge: function (birth_year) {
+        return prompt('Enter a year') - birth_year;
+    }
+    */
+/*
+calcAge: function () {
+ console.log(this);
+ return prompt('Enter a year') - this.birth_year;
+}
+};
+console.log(jonas.calcAge());
+// console.log(jonas['calcAge'](1991));
+*/
